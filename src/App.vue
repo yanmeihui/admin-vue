99d27ab876c8b0f4fff5286909efc137cdeac9ac
ymh <1608918347@qq.com>
2018-04-12 22:38:14 +0800

    添加登陆和home组建并且配置路由导航[C

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <p>conponent</p>
+    <router-view></router-view>
   </div>
 </template>
 
---
+++ src/components/login/login.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+  <p>登陆组建</p>
+</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style>
+</style>
